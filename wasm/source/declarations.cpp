8adbde903f5f10f2d5db4e91adb34bcd6a6d985b
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-11 16:23:59 +0200

    add clang-format and check-style workflow

--- wasm/source/declarations.cpp
+++ wasm/source/declarations.cpp
@@ -1,24 +1,26 @@
-#include "wasmGlue.h"
-
 #include <glue/context.h>
 #include <glue/declarations.h>
+
 #include <iostream>
+
+#include "wasmGlue.h"
 
 int main() {
   auto glueValue = greeterGlue();
-  
+
   glue::Context context;
   context.addRootMap(glueValue);
 
   glue::DeclarationPrinter printer;
   printer.init();
-  
+
   printer.print(std::cout, glueValue, &context);
 
   // export declarations as PostRunModule (module type after main() has completed)
   std::cout << "\n\nexport type GlueModule = {\n";
-  for (auto key: glueValue.keys()) {
-    std::cout << "  " << key << ": " << "typeof " << key << ";\n";
+  for (auto key : glueValue.keys()) {
+    std::cout << "  " << key << ": "
+              << "typeof " << key << ";\n";
   }
   std::cout << "}\n";
 
@@ -33,7 +35,8 @@ int main() {
 
   // add emscripten module export
   std::cout << "export type PostRunModule = GlueModule & EmGlueModule & EmscriptenModule\n";
-  std::cout << "export type PreRunModule = { then(arg: (module: PostRunModule) => void): PreRunModule }\n";
+  std::cout << "export type PreRunModule = { then(arg: (module: PostRunModule) => void): "
+               "PreRunModule }\n";
   std::cout << "export default function(): PreRunModule";
   std::cout << std::endl;
 

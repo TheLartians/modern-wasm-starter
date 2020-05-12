7417d95975d4031f8a457bcd3739a9a809dd2b76
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-13 01:37:57 +0200

    update EmGlue

--- source/wasmWrapper.ts
+++ source/wasmWrapper.ts
@@ -1,11 +1,15 @@
 import wasmLoader, {
-  PreRunModule,
-  PostRunModule,
   GlueModule,
+  PostRunModule,
+  PreRunModule,
 } from "./WasmModule.js";
+
 export { GlueModule } from "./WasmModule.js";
 
-type Deletable = { delete(): void };
+type Deletable = {
+  delete(): void;
+};
+
 type Scope = Deletable[];
 
 const scopeStack: Scope[] = [];
@@ -49,7 +53,8 @@ function getWasm() {
             module.callMain();
             module.setConstructCallback((v) => currentScope()?.push(v));
             wasmModule = module;
-            // remove `then` propery to resolve promise without creating an endless loop
+            // remove `then` property to resolve promise without creating an
+            // endless loop
             (loaded as any)["then"] = undefined;
           }
           res(wasmModule);
@@ -75,14 +80,19 @@ export function __getCurrentWasmScopeStackSize() {
 }
 
 /**
- * persist a value from the current scope so it will stay alive after the scope is closed
+ * persist a value from the current scope so it will stay alive after the scope
+ * is closed
  * @param value: the wasm value to be persisted
  */
 export function persistWasmValue<V>(value: V) {
   const scope = currentScope();
   if (scope) {
-    const result = scope.splice(scope.indexOf(value as any), 1);
-    if (result.length == 0) {
+    const idx = scope.indexOf(
+      (value as any)["__glue_instance"]
+    );
+    if (idx != -1) {
+      scope.splice(idx, 1);
+    } else {
       console.error(
         `could not persist value: value not found in current scope.`
       );
@@ -122,7 +132,8 @@ export function withWasmScope<R>(callback: () => R) {
 }
 
 /**
- * Calls `callback` asynchronously inside a wasm scope with the wasm module as an argument
+ * Calls `callback` asynchronously inside a wasm scope with the wasm module as
+ * an argument
  * @param callback
  */
 export async function withWasm<R>(callback: (module: GlueModule) => R) {

dfb44a4f746afa9f3b0b506d29b6b0f4edbaa9ff
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-28 14:18:21 +0200

    automatically update CPM.cmake on version change

--- wasm/package-lock.cmake
+++ wasm/package-lock.cmake
@@ -1,19 +1,19 @@
 # CPM Package Lock
 # This file should be committed to version control
 
-# PackageProject.cmake
-CPMDeclarePackage(PackageProject.cmake "NAME;PackageProject.cmake;GITHUB_REPOSITORY;TheLartians/PackageProject.cmake;VERSION;1.2")
 # Greeter
 CPMDeclarePackage(Greeter "NAME;Greeter;GITHUB_REPOSITORY;TheLartians/ModernCppStarter;VERSION;0.10")
-# EasyIterator
-CPMDeclarePackage(EasyIterator "NAME;EasyIterator;VERSION;1.4;GIT_REPOSITORY;https://github.com/TheLartians/EasyIterator.git")
-# StaticTypeInfo
-CPMDeclarePackage(StaticTypeInfo "NAME;StaticTypeInfo;VERSION;1.3.1;GIT_REPOSITORY;https://github.com/TheLartians/StaticTypeInfo")
-# Revisited
-CPMDeclarePackage(Revisited "NAME;Revisited;VERSION;2.3.1;GITHUB_REPOSITORY;TheLartians/Revisited")
-# Glue
-CPMDeclarePackage(Glue "NAME;Glue;GITHUB_REPOSITORY;TheLartians/Glue;VERSION;1.5")
+# PackageProject.cmake
+CPMDeclarePackage(PackageProject.cmake "NAME;PackageProject.cmake;GITHUB_REPOSITORY;TheLartians/PackageProject.cmake;VERSION;1.2")
 # EmGlue
 CPMDeclarePackage(EmGlue "NAME;EmGlue;VERSION;0.6.1;GITHUB_REPOSITORY;TheLartians/EmGlue")
+# Glue
+CPMDeclarePackage(Glue "NAME;Glue;GITHUB_REPOSITORY;TheLartians/Glue;VERSION;1.5")
+# EasyIterator
+CPMDeclarePackage(EasyIterator "NAME;EasyIterator;VERSION;1.4;GIT_REPOSITORY;https://github.com/TheLartians/EasyIterator.git")
+# Revisited
+CPMDeclarePackage(Revisited "NAME;Revisited;VERSION;2.3.1;GITHUB_REPOSITORY;TheLartians/Revisited")
+# StaticTypeInfo
+CPMDeclarePackage(StaticTypeInfo "NAME;StaticTypeInfo;VERSION;1.3.1;GIT_REPOSITORY;https://github.com/TheLartians/StaticTypeInfo")
 # Format.cmake
 CPMDeclarePackage(Format.cmake "NAME;Format.cmake;GITHUB_REPOSITORY;TheLartians/Format.cmake;VERSION;1.4")
